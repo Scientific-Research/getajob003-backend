80b1ac863e20de2bb2322198dce374e2258f7ec7
Maximilian <scientificresearch309@gmail.com>
2023-05-05 23:36:22 +0200

    show jobs with einfacher HTML with back tick

--- dist/model.js
+++ dist/model.js
@@ -2,7 +2,9 @@ export const getApiDocumentationHtml = () => {
     return `
   
   <h1>GET A JOB API</h1>
-  
+  <ul>
+  <li><a href="jobs">/jobs</a> - returns an array of job objects</li>
+  </ul>
   `;
 };
 //# sourceMappingURL=model.js.map
--- src/model.ts
+++ src/model.ts
@@ -2,6 +2,8 @@ export const getApiDocumentationHtml = () => {
   return `
   
   <h1>GET A JOB API</h1>
-  
+  <ul>
+  <li><a href="jobs">/jobs</a> - returns an array of job objects</li>
+  </ul>
   `;
 };

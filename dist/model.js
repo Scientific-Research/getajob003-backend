115ce9cce4c4b79a09e3ed66963cbc3450cb2f3b
Maximilian <scientificresearch309@gmail.com>
2023-05-06 01:23:25 +0200

    jobs have empty skills

--- dist/model.js
+++ dist/model.js
@@ -1,11 +1,19 @@
 import fs from "fs";
-const jobs = JSON.parse(fs.readFileSync("./src/data/jobs.json", "utf8"));
+const _jobs = JSON.parse(fs.readFileSync("./src/data/jobs.json", "utf8"));
 const skillInfos = JSON.parse(fs.readFileSync("./src/data/skillInfos.json", "utf8"));
 export const getJobs = () => {
+    const jobs = [];
+    _jobs.forEach((_job) => {
+        const job = {
+            ..._job,
+            skills: [],
+        };
+        jobs.push(job);
+    });
     return jobs;
 };
 export const getTodos = () => {
-    return jobs.map((job) => {
+    return _jobs.map((job) => {
         return {
             todo: job.todo,
             company: job.company,
--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,12 @@
 import fs from "fs";
 
+type Skill = {
+  idCode: string;
+  name: string;
+  url: string;
+  description: string;
+};
+
 type Job = {
   id: number;
   title: string;
@@ -7,21 +14,32 @@ type Job = {
   url: string;
   description: string;
   skilList: string;
+  skills: Skill[];
   todo: string;
 };
 
-const jobs: Job[] = JSON.parse(fs.readFileSync("./src/data/jobs.json", "utf8"));
+const _jobs: Job[] = JSON.parse(
+  fs.readFileSync("./src/data/jobs.json", "utf8")
+);
 
 const skillInfos: any = JSON.parse(
   fs.readFileSync("./src/data/skillInfos.json", "utf8")
 );
 
 export const getJobs = () => {
+  const jobs: Job[] = [];
+  _jobs.forEach((_job) => {
+    const job: Job = {
+      ..._job,
+      skills: [],
+    };
+    jobs.push(job);
+  });
   return jobs;
 };
 
 export const getTodos = () => {
-  return jobs.map((job) => {
+  return _jobs.map((job) => {
     return {
       todo: job.todo,
       company: job.company,
